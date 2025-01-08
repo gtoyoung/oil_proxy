85394cd06d2124e020c749d24a0390bd2e35304d
ban <gtoyoung@naver.com>
2025-01-08 13:45:57 +0900

    수정

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 80;
 const OIL_CODE = process.env.OIL_CODE;
 const app = express();
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
-const model = genAI.getGenerativeModel({model: "gemini-2.0-flash-exp",});
+const systemInstruction = "당신은 건방진 태도를 가지는 조무진이라는 친구입니다.\nMBTI는 ISFP이고, "
+ + "사용자는 친한 친구입니다. 츤데레처럼 제시한 정보를 토대로 주유소 이름, 거리, 가격을 포함하여 5문장 이내로 짧게 대답해주세요.";
+const model = genAI.getGenerativeModel({model: "gemini-2.0-flash-exp", systemInstruction: systemInstruction});
 
 const corsOptions = {
   origin: process.env.TARGET_URL,
@@ -32,7 +34,7 @@ app.get("/getTop20", function (req, res) {
 app.get("/createOilInfo", async function (req, res) {
   try {
     const message = req.param("message");
-    const prompt = `주유소 정보는 ${message}입니다.\n\n위의 주유소 가격과 거리 정보를 통해서 가장 기름넣으러가기 적절한 주유소를 추천해줘.\n\n답변 형태는 아래와 같이 답변해줘.\n\n1. 주유소 이름\n2. 가격 정보\n3. 거리 정보`;
+    const prompt = `주유소 정보는 ${message}입니다.\n\n이정보를 토대로 주유소를 추천해줘.`;
 
     // Set headers for SSE
     res.setHeader('Content-Type', 'text/event-stream');
